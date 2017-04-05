405a2bfc7a1041bdfecd069c10fc498c4ba932c7
dinno1986 <dimitri.castell@hotmail.fr>
2017-04-05 12:09:38 +0200

    recuperation du localstorage et s'affiche dans textarea

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
+
 	$("#enregistrer").on('click',function(){   // quand je clique la valeur s'enregistre
 		var valeur= $("#saisie").val();
-		localStorage.setItem("string",valeur);
-$("#saisie").val("");
+		localStorage.setItem("save",valeur);   // et localStorage l'enregistre
+		$("#saisie").val("");
+		window.location.reload();
+		
 	});
+var recup = localStorage.getItem("save");  //récupérer la valeur enregistrer de local storage
+
+$("textarea").val(recup);
+console.log(recup);	
 
 });
+
+
+
 
 
